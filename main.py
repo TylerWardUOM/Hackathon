0ff7604d2653e6c9af4bfe524d7522b00a403052
TylerWardUOM <tyler.ward@student.manchester.ac.uk>
2025-03-29 21:17:20 +0000

    tapInHandler

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
-import requests
-
+import scripts.apiRequests as apiRequests
+import time
 # Flask API URL (Running on the same Raspberry Pi)
 BASE_URL = "http://127.0.0.1:5000"  # Change to Pi's IP if calling externally
 #BASE_URL = "http://10.209.195.140:5000"  # Change to Pi's IP if calling externally
 
 # Test the API with a GET request
-response = requests.get(f"{BASE_URL}/status")
-print("Response from /status:", response.json())
 
-# Send data using a POST request
-data = {"studentName": "Tyler"}
-response = requests.post(f"{BASE_URL}/attend", json=data)
-print("Response from /attend:", response.json())
-
-
-response = requests.get(f"{BASE_URL}/attendance")
-print("Response from /attendance:", response.json())
-
-data = {"studentName": "Tyler"}
-response = requests.post(f"{BASE_URL}/verify", json=data)
-print("Response from /verify:", response.json())
-
-response = requests.get(f"{BASE_URL}/attendance")
-print("Response from /attendance:", response.json())
+while True:
+    apiRequests.getAttendance()
+    time.sleep(0.5)
---
+++ scripts/apiRequests.py
@@ -0,0 +1,21 @@
+import requests
+
+BASE_URL = "http://127.0.0.1:5000"  # Change to Pi's IP if calling externally
+
+def attend_student(name):
+    data = {"studentName": name}
+    response = requests.post(f"{BASE_URL}/attend", json=data)
+    print("Response from /attend:", response.json())
+
+def getStatus():
+    response = requests.get(f"{BASE_URL}/status")
+    print("Response from /status:", response.json())
+
+def getAttendance():
+    response = requests.get(f"{BASE_URL}/attendance")
+    print("Response from /attendance:", response.json())
+
+def verifyStudent():
+    data = {"studentName": "Tyler"}
+    response = requests.post(f"{BASE_URL}/verify", json=data)
+    print("Response from /verify:", response.json())
